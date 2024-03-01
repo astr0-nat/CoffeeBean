8682125f8e708a1c4122a81e3874227bf9100fce
Natalie Pentschev <npentschev@berkeley.edu>
2024-03-01 14:44:46 -0800

    new design that authenticates with the Google Groups API and gathers all groups of the company

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-import os
-import pickle
-# Gmail API utils
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-# for encoding/decoding messages in base64
-from base64 import urlsafe_b64decode, urlsafe_b64encode
-# for dealing with attachement MIME types
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-from email.mime.audio import MIMEAudio
-from email.mime.base import MIMEBase
-from mimetypes import guess_type as guess_mime_type
 
-# Request all access (permission to read/send/receive emails, manage the inbox, and more)
-SCOPES = ['https://mail.google.com/']
-our_email = 'pentnat@gmail.com'
+SERVICE_ACCOUNT_FILE = "./m2m_digest_service_key.json"
+SCOPES = ['https://www.googleapis.com/auth/gmail.send',
+          'https://www.googleapis.com/auth/gmail.readonly',
+          'https://www.googleapis.com/auth/admin.directory.group.readonly',
+          'https://www.googleapis.com/auth/admin.directory.group']
+DOMAINS = ['greenlandpropertycare.com', 'month2month.com', 'holidale.com']
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+delegated_credentials = credentials.with_subject('summary@month2month.com')
 
-# loads the credentials.json, does the authentication with Gmail API and returns a service object
-# that will be used in all upcoming functions
-def gmail_authenticate():
-    creds = None
-    # the file token.pickle stores the user's access and refresh tokens, and is created
-    # automatically when the authorization flow completes for the first time
-    if os.path.exists('token.pickle'):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
-    # if there are no (valid) credentials available, let the user log in
-    if not creds or not creds.valid:
-        # if the access token needs refreshed
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else: flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-        # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-    return build('gmail', 'v1', credentials=creds)
+def get_group_service():
 
-# get the Gmail API Service
-service = gmail_authenticate()
+# Build the service object for the Admin SDK Directory API using the authorized credentials
+group_service = build('admin', 'directory_v1', credentials=delegated_credentials)
+
+# Build the service object for the gmail API using the authorized credentials
+gmail_service = build('gmail', 'v1', credentials=delegated_credentials)
+
+# Make a request to list all groups in each domain
+all_groups = []
+for domain in DOMAINS:
+    request = group_service.groups().list(domain=domain)
+    while request is not None:
+        response = request.execute()
+        groups = response.get('groups', [])
+        for group in groups:
+            all_groups.append(group)
+        request = group_service.groups().list_next(previous_request=request, previous_response=response)
+
+# Now `all_groups` contains groups from all domains
+for group in all_groups:
+    print(f"Group Name: {group['name']}, Email: {group['email']}")
+
+
+
