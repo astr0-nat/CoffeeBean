39e87785480c4f8055bd0ae58728a7bf845dee0f
Natalie Pentschev <npentschev@berkeley.edu>
2024-03-25 22:12:21 -0700

    updated docstring of group_summary_extractor

--- group_extractor/company_group_extractor.py
+++ group_extractor/company_group_extractor.py
@@ -4,54 +4,52 @@ import pickle
 
 def extract_email_addresses_from_file(source_file, dest_file):
     """
-    Extract unique Google group email addresses from a text file.
+       Extracts and processes email addresses from a text file to create a set of unique, lowercase email addresses.
 
-    Reads a provided text file, identifies all unique email addresses within, and outputs a pickle file of
-    a Python set of these unique email addresses.
+       This function is specifically designed for handling the extraction of email addresses from the text content
+       typically obtained from https://groups.google.com/ using the summary@month2month.com account. It reads a
+       provided text file, identifies all email addresses using a regex pattern, adds specific email addresses
+       ('allstaff@month2month.com' and 'team-leads@month2month.com'), converts all email addresses to lowercase,
+       and outputs a pickle file containing a Python set of these processed email addresses. This set includes unique
+       email addresses in a normalized format, suitable for various applications that require a list of email addresses.
 
-    Parameters:
-        source_file (str): The path to the text file containing the input text.
-        dest_file (str): The path to the output set pickle file.
-        This file is ./text_from_all_groups_website.txt which is all the text copied and pasted
-        from https://groups.google.com/my-groups.
+       Parameters:
+           source_file (str): The path to the text file containing the input text. This file typically contains
+           text copied from the Google Groups web interface for the summary@month2month.com account.
+           dest_file (str): The path to the output file where the set of email addresses will be saved as a pickle file.
 
-    Example:
-        >>> extract_email_addresses_from_file("text_from_all_groups_website.txt")
-        '{"example@test.com"}'
+       Example Usage:
+           >>> extract_email_addresses_from_file("text_from_all_groups_website.txt", "google_groups_set.pkl")
 
-    Purpose:
-        PyGroupDigest Google group summarizer needs access to all current Google Group email addresses within the company. This script aims to
-        provide an easy way to keep the PyGroupDigest updated with any new Group additions.
+       Purpose:
+           Designed to assist in maintaining an updated list of Google Group email addresses, including additional,
+           specific email addresses for applications like PyGroupDigest. It streamlines the process of updating email
+           lists by automating the extraction and normalization of email addresses from text files.
 
-    Usage:
-        When a new Google Group needs to be summarizes, summary@month2month.com will first be added as a group member. This will be refelected in
-        the /my-groups page of the Google Groups website for user sumary@month2month.com.
+       Note:
+           This function adds 'allstaff@month2month.com' and 'team-leads@month2month.com' to the extracted emails list
+           as they are important for summarization but might not be listed in the source file.
 
-        If ever summary@gmail.com is added to a new Google Group, simply add it to the ./text_from_all_groups_website.txt text file within this directory.
-
-        In the event you are unsure which groups are new or not, simply delete all the text within the ./text_from_all_groups_website.txt text file,
-        navigate to the https://groups.google.com/my-groups website, select all the text available, while making sure to include all email addresses,
-        and paste it into the ./text_from_all_groups_website.txt text file within this directory. Navigate to any additional pages on the Groups website
-        if necessary and continue to copy and paste that text to the same text file.
-
-        Then run this script while in the home directory of this script and the text file, and copy the output to the ALL_COMPANY_GOOGLE_GROUPS variable in the .env
-        file in ithe PyGroupDigest directory
-
-        """
+       Usage Guide:
+           1. Log in to https://groups.google.com/ with the summary@month2month.com account and copy the webpage text
+              that contains the email addresses.
+           2. Save this text to the source file.
+           3. Run this function specifying the source and destination file paths.
+           4. Use the output pickle file in your applications as needed, for instance, to update the
+              ALL_COMPANY_GOOGLE_GROUPS variable in a configuration file for email-related processing.
+       """
     with open(source_file, 'r') as file:
         text = file.read()
 
-    # Regular expression to match email addresses
     email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-
-    # Find all matches in the text
     email_addresses = re.findall(email_pattern, text)
-
-    # Convert the list of email addresses to a set to remove duplicates
-    unique_email_addresses = set(email_addresses)
+    # allstaff is not a Google Group, but it is good to summarize
+    email_addresses.append("allstaff@month2month.com")
+    email_addresses.append("team-leads@month2month.com")
+    lowercase_emails = set(map(lambda email: email.lower(), email_addresses))
 
     with open(dest_file, 'wb') as dest_file:
-        pickle.dump(unique_email_addresses, dest_file)
+        pickle.dump(lowercase_emails, dest_file)
 
 
 source_file_path = "text_from_all_groups_website.txt"
