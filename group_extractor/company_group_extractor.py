01f496799823c7ce6e0f05a6db08c47aabbcb5d7
Natalie Pentschev <npentschev@berkeley.edu>
2024-03-26 10:56:49 -0700

    cleaned up docstring for company_group_extractor.py and changed the file name of text_from_all_groups_website.txt to text_from_all_groups_webpage.txt

--- group_extractor/company_group_extractor.py
+++ group_extractor/company_group_extractor.py
@@ -4,40 +4,44 @@ import pickle
 
 def extract_email_addresses_from_file(source_file, dest_file):
     """
-       Extracts and processes email addresses from a text file to create a set of unique, lowercase email addresses.
+        Extracts and processes email addresses from a text file to create a set of unique, lowercase email addresses.
 
-       This function is specifically designed for handling the extraction of email addresses from the text content
-       typically obtained from https://groups.google.com/ using the summary@month2month.com account. It reads a
-       provided text file, identifies all email addresses using a regex pattern, adds specific email addresses
-       ('allstaff@month2month.com' and 'team-leads@month2month.com'), converts all email addresses to lowercase,
-       and outputs a pickle file containing a Python set of these processed email addresses. This set includes unique
-       email addresses in a normalized format, suitable for various applications that require a list of email addresses.
+        This function is tailored for extracting email addresses from text content obtained from https://groups.google.com/
+        using the summary@month2month.com account. It reads a provided text file, identifies all email addresses using a
+        regex pattern, adds specific email addresses ('allstaff@month2month.com' and 'team-leads@month2month.com'),
+        converts all email addresses to lowercase, and outputs a pickle file containing a Python set of these processed
+        email addresses. The set includes unique email addresses in a normalized format, ready for use in applications
+        requiring a list of email addresses, such as the CoffeeBean Gmail summarizer.
 
-       Parameters:
-           source_file (str): The path to the text file containing the input text. This file typically contains
-           text copied from the Google Groups web interface for the summary@month2month.com account.
-           dest_file (str): The path to the output file where the set of email addresses will be saved as a pickle file.
+        Parameters:
+            source_file (str): The path to the text file containing the input text. This file typically includes text
+            copied from the Google Groups web interface for the summary@month2month.com account.
+            dest_file (str): The path to the output file where the set of email addresses will be saved as a pickle file.
 
-       Example Usage:
-           >>> extract_email_addresses_from_file("text_from_all_groups_website.txt", "google_groups_set.pkl")
+        Example Usage:
+            >>> extract_email_addresses_from_file("text_from_all_groups_webpage.txt", "google_groups_set.pkl")
 
-       Purpose:
-           Designed to assist in maintaining an updated list of Google Group email addresses, including additional,
-           specific email addresses for applications like PyGroupDigest. It streamlines the process of updating email
-           lists by automating the extraction and normalization of email addresses from text files.
+        Purpose:
+            The script aids in keeping an up-to-date list of Google Group email addresses, plus additional specific
+            email addresses, for applications like the CoffeeBean Gmail summarizer. It automates the extraction and
+            normalization of email addresses from text files, facilitating the updating process of email lists.
 
-       Note:
-           This function adds 'allstaff@month2month.com' and 'team-leads@month2month.com' to the extracted emails list
-           as they are important for summarization but might not be listed in the source file.
+        Note:
+            The function explicitly includes 'allstaff@month2month.com' and 'team-leads@month2month.com' to the list
+            of extracted emails as they are deemed crucial for summarization purposes but might not be present in the
+            source file.
 
-       Usage Guide:
-           1. Log in to https://groups.google.com/ with the summary@month2month.com account and copy the webpage text
-              that contains the email addresses.
-           2. Save this text to the source file.
-           3. Run this function specifying the source and destination file paths.
-           4. Use the output pickle file in your applications as needed, for instance, to update the
-              ALL_COMPANY_GOOGLE_GROUPS variable in a configuration file for email-related processing.
-       """
+        When to Run:
+            - Whenever a new Google Group needs to be summarized, summary@month2month.com must be added to the group. In
+            this case, this script must be run according to the usage guide.
+
+        Usage Guide:
+            1. Log into https://groups.google.com/ with the summary@month2month.com account and copy the webpage text that
+               contains the email addresses.
+            2. Save this text into the source file.
+            3. Run this script with the specified source and destination file paths to update the list of email addresses.
+            4. This output pickle file is utilized by the company_google_groups set in CoffeeBean.
+        """
     with open(source_file, 'r') as file:
         text = file.read()
 
@@ -52,6 +56,6 @@ def extract_email_addresses_from_file(source_file, dest_file):
         pickle.dump(lowercase_emails, dest_file)
 
 
-source_file_path = "text_from_all_groups_website.txt"
+source_file_path = "text_from_all_groups_webpage.txt"
 dest_file_path = "google_groups_set.pkl"
 extract_email_addresses_from_file(source_file_path, dest_file_path)
